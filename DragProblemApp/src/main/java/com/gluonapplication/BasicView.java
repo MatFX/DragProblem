5f6a501fedf3a097f4dd1d9e42189e9268f783a8
M.Goerlich <M.Goerlich@bscgmbh.de>
2019-06-28 11:03:07 +0200

    /**/

--- DragProblemApp/src/main/java/com/gluonapplication/BasicView.java
+++ DragProblemApp/src/main/java/com/gluonapplication/BasicView.java
@@ -9,13 +9,7 @@ import com.gluonapplication.item.CardPaneItem;
 import com.gluonhq.charm.glisten.control.AppBar;
 import com.gluonhq.charm.glisten.control.CardCell;
 import com.gluonhq.charm.glisten.control.CardPane;
-import com.gluonhq.charm.glisten.control.Icon;
 import com.gluonhq.charm.glisten.mvc.View;
-import com.gluonhq.charm.glisten.visual.MaterialDesignIcon;
-import javafx.geometry.Pos;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
 
 public class BasicView extends View {
 	
